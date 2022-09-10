5e1df6c1620451d70d3fe1f5b6dcf9a6a3b81a5f
Minh Nhật Mai <nhat.maik20dhbk@hcmut.edu.vn>
2022-09-11 00:04:02 +0700

    Fixed mobile sub-menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,35 @@ function sliderFn() {
     setTimeout(sliderFn, 5000);
 }
 
+/* Mobile menu JavaScript */
+var header = document.getElementById('header');
+var menuBtn = document.getElementById('js-menu-btn');
+var headerHeight = header.clientHeight;
+
+function showHideMenu() {
+    var isClosed = header.clientHeight === headerHeight;
+    if(isClosed) { 
+        header.style.height = 'auto'; 
+    } else { 
+        header.style.height = null;
+    }
+}
+
+menuBtn.onclick = showHideMenu;
+
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+
+for(var menuItem of menuItems) {
+    menuItem.onclick = function(event) {
+        var isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
+        if(!isParentMenu) {
+            header.style.height = null;
+        } else {
+            event.preventDefault();
+        }
+    }
+}
+
 /* Modal JavaScript */
 const buyBtns = document.querySelectorAll('.js-buy-tickets');
 const modal = document.querySelector('.js-modal');
@@ -36,4 +65,4 @@ modal.addEventListener('click', hideBuyTickets);
 
 modalContainer.addEventListener('click', function(event) {
     event.stopPropagation();
-} );
+});
